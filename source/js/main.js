93fe69f2d79d13383b02a9d1d4dd7457950de8a1
Alexandr Zawgorodniy <alexandr.zawgorodniy@gmail.com>
2021-08-11 18:44:58 +0700

    feat: add js for mobile menu

---
+++ source/js/main.js
@@ -0,0 +1,22 @@
+'use strict';
+document.body.classList.remove('no-js');
+const menuBtn = document.querySelector('.menu-control');
+const menuIcons = menuBtn.querySelectorAll('.menu-control__icon');
+const menuBtnSvg = menuBtn.querySelector('svg');
+const navMenu = document.querySelector('.main-header__nav');
+const navClosed = 'main-header__nav--closed';
+const menuBtnClosed = 'menu-control--close';
+const menuIconActive = 'menu-control__icon--active';
+navMenu.classList.add(navClosed);
+
+const toggleMenu = () => {
+  navMenu.classList.toggle(navClosed);
+  menuBtn.classList.toggle(menuBtnClosed);
+  menuIcons.forEach((icon) => icon.classList.toggle(menuIconActive));
+  const currentWidth = menuBtn.querySelector(`.${menuIconActive}`).getAttribute('width');
+  const currentHeight = menuBtn.querySelector(`.${menuIconActive}`).getAttribute('height');
+  menuBtnSvg.style.cssText = `width: ${currentWidth}px; height: ${currentHeight}px`;
+};
+
+menuBtn.addEventListener('click', toggleMenu);
+
